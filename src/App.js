e12fe06494409d5c5e44888892be8cb2483603a3
Octavio Fuentes <tavofuentes@gmail.com>
2024-04-05 12:51:03 -0600

    feat: add navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import Home from './routes/home/home.component';
-import { Route, Routes } from 'react-router-dom';
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import { Route, Routes } from "react-router-dom";
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />}></Route>
+        <Route path="shop" element={<Shop />}></Route>
+      </Route>
     </Routes>
   );
-}
+};
 
 export default App;
